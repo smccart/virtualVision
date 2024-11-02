d60bcf44ff20a0848ad18aa3a5a502a7875c54a0
Abe Reyes <abe.raise@gmail.com>
2024-11-01 20:35:36 -0400

    Refactor: Move hero section from MainLayout to ChoicePage for improved layout and organization.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -63,59 +63,46 @@
           to="/"
           class="custom-drawer-item"
         >
-          <q-item-section class="custom-drawer-item-section">
-            {{ homeLabel }}
-          </q-item-section>
+          <q-item-section class="custom-drawer-item-section">{{
+            homeLabel
+          }}</q-item-section>
         </q-item>
         <q-item
           to="/about"
           class="custom-drawer-item"
         >
-          <q-item-section class="custom-drawer-item-section">
-            {{ aboutLabel }}
-          </q-item-section>
+          <q-item-section class="custom-drawer-item-section">{{
+            aboutLabel
+          }}</q-item-section>
         </q-item>
         <q-item
           to="/services"
           class="custom-drawer-item"
         >
-          <q-item-section class="custom-drawer-item-section">
-            {{ servicesLabel }}
-          </q-item-section>
+          <q-item-section class="custom-drawer-item-section">{{
+            servicesLabel
+          }}</q-item-section>
         </q-item>
         <q-item
           to="/portfolio"
           class="custom-drawer-item"
         >
-          <q-item-section class="custom-drawer-item-section">
-            {{ portfolioLabel }}
-          </q-item-section>
+          <q-item-section class="custom-drawer-item-section">{{
+            portfolioLabel
+          }}</q-item-section>
         </q-item>
         <q-item
           to="/contact"
           class="custom-drawer-item"
         >
-          <q-item-section class="custom-drawer-item-section">
-            {{ contactLabel }}
-          </q-item-section>
+          <q-item-section class="custom-drawer-item-section">{{
+            contactLabel
+          }}</q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
 
     <q-page-container>
-      <div class="hero-section">
-        <h1 class="hero-title">{{ heroTitle }}</h1>
-        <div class="hero-buttons">
-          <q-btn
-            :label="learnMoreLabel"
-            class="learn-more-btn"
-          />
-          <q-btn
-            :label="exploreLabel"
-            class="explore-btn"
-          />
-        </div>
-      </div>
       <router-view />
     </q-page-container>
   </q-layout>
@@ -140,17 +127,12 @@
 
   const toolbarTitle = 'Vision 2 Virtual';
   const tagline = 'Bringing Your Vision to Life';
-  const heroTitle =
-    'Your dreams matter, and we bring bold ideas to life. Our engaging websites captivate your audience while delivering thoughtful, compelling solutions.';
 
   const homeLabel = 'Home';
   const aboutLabel = 'About Us';
   const servicesLabel = 'Services';
   const portfolioLabel = 'Portfolio';
   const contactLabel = 'Contact';
-
-  const learnMoreLabel = 'Learn More';
-  const exploreLabel = 'Explore';
 </script>
 
 <style scoped lang="scss">
@@ -205,66 +187,5 @@
     max-width: 180px !important;
     overflow: hidden;
     white-space: nowrap;
-  }
-
-  .hero-section {
-    padding: 1rem 7rem 0;
-    text-align: center;
-    background-color: rgba($orangeLight, 0.01);
-    min-height: 300px;
-    color: $blueDark;
-    max-width: 100%;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);
-  }
-
-  .hero-title {
-    color: $blueDark;
-    font-size: 1.9rem;
-    font-weight: 700;
-    letter-spacing: 0.0001em;
-    line-height: 2.2;
-  }
-
-  .hero-buttons {
-    display: flex;
-    justify-content: center;
-    gap: 4rem;
-    padding: 1rem;
-    margin-bottom: 2rem;
-  }
-
-  .learn-more-btn {
-    background-color: $redDark;
-    color: mix($textWhite, $greenVeryLight, 99%);
-    border-radius: 999px;
-    padding: 0.5rem 1.5rem;
-    font-size: 1.2rem;
-    width: 240px;
-    text-align: center;
-    white-space: nowrap;
-    transition: transform 0.3s ease, box-shadow 0.3s ease,
-    background-color 0.3s ease;
-  }
-
-  .explore-btn {
-    background-color: $blueDark;
-    color: mix($textWhite, $orangeVeryLight, 99%);
-    border-radius: 999px;
-    padding: 0.5rem 1.5rem;
-    font-size: 1.2rem;
-    width: 240px;
-    text-align: center;
-    white-space: nowrap;
-    transition: transform 0.3s ease, box-shadow 0.3s ease,
-    background-color 0.3s ease;
-  }
-
-  .q-btn:hover {
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    transform: translateY(-1px);
   }
 </style>
